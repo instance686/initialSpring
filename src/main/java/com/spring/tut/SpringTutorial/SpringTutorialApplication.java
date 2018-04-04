175f0774b439087ca808187fb904e0765e6fb64d
ayush <ayush.cir@gmail.com>
2018-04-05 01:02:15 +0530

    AppicationContext and property in xml

---
+++ src/main/java/com/spring/tut/SpringTutorial/SpringTutorialApplication.java
@@ -0,0 +1,21 @@
+package com.spring.tut.SpringTutorial;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTutorialApplication.class, args);
+			//BeanFactory factory=new ClassPathXmlApplicationContext("spring.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+			Triangle triangle=(Triangle)context.getBean("triangle");
+			triangle.draw();
+	}
+}
