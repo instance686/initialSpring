ae7e4eb634312ec6596d3f5d68803444a24fa2c8
ayush <ayush.cir@gmail.com>
2018-04-05 15:33:22 +0530

    BeanFactoryPostProcessor

---
+++ src/main/java/com/spring/tut/SpringTutorial/MyBeanFactory.java
@@ -0,0 +1,15 @@
+package com.spring.tut.SpringTutorial;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+public class MyBeanFactory implements BeanFactoryPostProcessor{
+
+	@Override
+	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+		// TODO Auto-generated method stub
+		System.out.println("My Bean Factory Post Processor  is called");
+	}
+
+}
